18f2538443fc6f5eec8d3c367c8fa1a51e2b3e00
Kathryn Teall <katgrace0808@gmail.com>
2020-05-24 15:12:48 -0700

    Questions added

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,8 @@
+// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(email, id) {
+        this.id = id;
+        this.email = email;
+    }
+}
+module.exports = Employee;
